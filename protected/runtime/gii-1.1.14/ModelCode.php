2a7d0d03b85174d241f2f09434e8111a8812339d
Fabio Migliorini <fabio.migliorini@gmail.com>
2019-06-26 18:00:50 -0400

    [FIX] pasta runtime

---
+++ protected/runtime/gii-1.1.14/ModelCode.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'template' => 'default',
+  'connectionId' => 'db',
+  'tablePrefix' => 'tbl',
+  'modelPath' => 'application.models',
+  'baseClass' => 'MGActiveRecord',
+  'buildRelations' => '1',
+  'commentsAsLabels' => '1',
+);
