5221cdcb5cd4ada2fcc2d1715ed990153f2ac81f
Fabio Migliorini <fabio.migliorini@gmail.com>
2021-07-03 16:41:34 -0400

    [ADD] Geracao Automatica Boletos BB

--- protected/models/Negocio.php
+++ protected/models/Negocio.php
@@ -346,6 +346,7 @@ class Negocio extends MGActiveRecord
             return false;
         }
 
+        $ret = $this->registrarBoletos();
         return $this->movimentaEstoque();
     }
 
@@ -758,6 +759,18 @@ class Negocio extends MGActiveRecord
             return false;
         }
         return true;
+    }
+
+    public function registrarBoletos()
+    {
+        $url = MGSPA_API_URL . "negocio/{$this->codnegocio}/boleto-bb/registrar";
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
     }
 
     public function valorPagamento()
