ff687d516396b0cab8b97a4bba30abc78f93f676
Fabio Migliorini <fabio@mgpapelaria.com.br>
2022-01-16 01:53:16 -0400

    [FIX] conexao com outros aplicativos da MG

--- protected/models/Negocio.php
+++ protected/models/Negocio.php
@@ -755,7 +755,12 @@ class Negocio extends MGActiveRecord
     {
         // Chama MGLara para fazer movimentacao do estoque com delay de 10 segundos
         $url = MGLARA_URL . "estoque/gera-movimento-negocio/{$this->codnegocio}?delay=10";
-        $ret = json_decode(file_get_contents($url));
+        $ret = json_decode(file_get_contents($url, false, stream_context_create([
+                "ssl" => [
+                        "verify_peer"=>false,
+                        "verify_peer_name"=>false,
+                ]
+        ])));
         if (@$ret->response !== 'Agendado') {
             echo '<pre>';
             var_dump($ret);
@@ -773,6 +778,8 @@ class Negocio extends MGActiveRecord
         curl_setopt($curl, CURLOPT_POST, true);
         // curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($curl);
         curl_close($curl);
         return $response;
