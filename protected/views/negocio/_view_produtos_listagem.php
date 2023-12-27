ac71b3e3996ef4c3dc0156d1a8f579dcd1d57644
Fabio Migliorini <fabio@mgpapelaria.com.br>
2023-12-27 11:38:55 -0400

    [UPD] Bloqueio Edicao Caso seja venda de PDV

--- protected/views/negocio/_view_produtos_listagem.php
+++ protected/views/negocio/_view_produtos_listagem.php
@@ -6,10 +6,10 @@ foreach ($model->NegocioProdutoBarras as $npb)
 	<div class="registro">
 		<span class="row-fluid">
 			<small class="span2 muted">
-				<?php echo CHtml::encode($npb->ProdutoBarra->barras) ?> 
+				<?php echo CHtml::encode($npb->ProdutoBarra->barras) ?>
 			</small>
 			<span class="span5">
-				<?php echo CHtml::link(CHtml::encode($npb->ProdutoBarra->descricao), array('produto/view', 'id'=> $npb->ProdutoBarra->codproduto)); ?> 
+				<?php echo CHtml::link(CHtml::encode($npb->ProdutoBarra->descricao), array('produto/view', 'id'=> $npb->ProdutoBarra->codproduto)); ?>
 				<?php
 					foreach ($npb->NegocioProdutoBarraDevolucaos as $npb_devolucao)
 					{
@@ -17,39 +17,39 @@ foreach ($model->NegocioProdutoBarras as $npb)
 						{
 							?>
 								<div class="label label-warning">
-									Devolvido <?php echo Yii::app()->format->formatNumber($npb_devolucao->quantidade, 3); ?> 
-									em 
+									Devolvido <?php echo Yii::app()->format->formatNumber($npb_devolucao->quantidade, 3); ?>
+									em
 									<?php echo CHtml::link(Yii::app()->format->formataCodigo($npb_devolucao->codnegocio), array('view', 'id'=>$npb_devolucao->codnegocio)); ?>
 								</div>
 							<?php
 						}
 					}
-					
+
 					if (!empty($npb->codnegocioprodutobarradevolucao))
 					{
 						?>
 							<div class="label label-warning">
-								Devolução referente Negócio 
+								Devolução referente Negócio
 								<?php echo CHtml::link(Yii::app()->format->formataCodigo($npb->NegocioProdutoBarraDevolucao->codnegocio), array('view', 'id'=>$npb->NegocioProdutoBarraDevolucao->codnegocio)); ?>
 							</div>
-						<?php			
+						<?php
 					}
-					
+
 				?>
 			</span>
 			<span class="span2 text-right">
 				<?php echo Yii::app()->format->formatNumber($npb->quantidade, 3); ?>  &nbsp;
 				<small class="pull-right muted">
-					<?php echo CHtml::encode($npb->ProdutoBarra->UnidadeMedida->sigla); ?> 
+					<?php echo CHtml::encode($npb->ProdutoBarra->UnidadeMedida->sigla); ?>
 				</small>
 			</span>
 			<span class="span1 text-right muted">
-				<?php echo Yii::app()->format->formatNumber($npb->valorunitario); ?> 
+				<?php echo Yii::app()->format->formatNumber($npb->valorunitario); ?>
 			</span>
 			<b class="span2 text-right">
-				<?php echo Yii::app()->format->formatNumber($npb->valortotal); ?> 
+				<?php echo Yii::app()->format->formatNumber($npb->valortotal); ?>
 				&nbsp;
-				<?php if ($model->codnegociostatus == 1): ?>
+				<?php if ($model->podeEditar()): ?>
 					<div class="pull-right">
 						<a href="<?php echo Yii::app()->createUrl('negocioProdutoBarra/update', array('id'=>$npb->codnegocioprodutobarra)); ?>"><i class="icon-pencil"></i></a>
 						<a class="delete-barra" href="<?php echo Yii::app()->createUrl('negocioProdutoBarra/delete', array('id'=>$npb->codnegocioprodutobarra, 'ajax'=>'ajax')); ?>"><i class="icon-trash"></i></a>
@@ -58,7 +58,7 @@ foreach ($model->NegocioProdutoBarras as $npb)
 			</b>
 		</span>
 		<?php
-		
+
 		?>
 	</div>
 
